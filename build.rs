01aa74ed334302660f6a4e1e62e25d72c48dac63
Connor Monks <monkslc@gmail.com>
2020-03-19 22:47:21 -0400

    add function to get languages based on interpreter used in shebang line

--- build.rs
+++ build.rs
@@ -11,16 +11,22 @@ use std::{
 #[derive(Deserialize)]
 struct Language {
     filenames: Option<Vec<String>>,
+    interpreters: Option<Vec<String>>,
 }
 
 fn main() {
-    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("filename-language-map.rs");
-    let mut file = BufWriter::new(File::create(&path).unwrap());
     let languages: HashMap<String, Language> =
         serde_yaml::from_str(&fs::read_to_string("languages.yml").unwrap()[..]).unwrap();
 
-    let mut filename_to_language_map = PhfMap::new();
+    create_filename_map(&languages);
+    create_interpreter_map(&languages);
+}
 
+fn create_filename_map(languages: &HashMap<String, Language>) {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("filename-language-map.rs");
+    let mut file = BufWriter::new(File::create(&path).unwrap());
+
+    let mut filename_to_language_map = PhfMap::new();
     for (language_name, language) in languages.iter() {
         if let Some(filenames) = &language.filenames {
             for filename in filenames.iter() {
@@ -34,6 +40,43 @@ fn main() {
         &mut file,
         "static FILENAMES: phf::Map<&'static str, &'static str> = \n{};\n",
         filename_to_language_map.build()
+    )
+    .unwrap();
+}
+
+fn create_interpreter_map(languages: &HashMap<String, Language>) {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("interpreter-language-map.rs");
+    let mut file = BufWriter::new(File::create(&path).unwrap());
+
+    let mut temp_map: HashMap<String, Vec<String>> = HashMap::new();
+    for (language_name, language) in languages.iter() {
+        if let Some(interpreters) = &language.interpreters {
+            for interpreter in interpreters.iter() {
+                match temp_map.get_mut(interpreter) {
+                    Some(entry) => {
+                        entry.push(language_name.clone());
+                    }
+                    None => {
+                        temp_map.insert(interpreter.clone(), vec![language_name.clone()]);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut interpreter_to_language_map = PhfMap::new();
+    for (interpreter, languages) in temp_map.iter() {
+        interpreter_to_language_map.entry(
+            &interpreter[..],
+            // split langauges with a | character
+            format!("\"{}\"", languages.join("|")).as_str(),
+        );
+    }
+
+    writeln!(
+        &mut file,
+        "static INTERPRETERS: phf::Map<&'static str, &'static str> = \n{};\n",
+        interpreter_to_language_map.build()
     )
     .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,120 @@
+use regex::Regex;
+use std::io::{self, BufRead, BufReader, Read};
+
 // Include the map from filenames to languages at compile time
 // static FILENAMES: phf::Map<&'static str, &'static str> = ...;
 include!(concat!(env!("OUT_DIR"), "/filename-language-map.rs"));
 
-pub fn get_language_by_filename(filename: &str) -> Option<&'static str> {
-    FILENAMES.get(filename).cloned()
+// Include the map from interpreters to languages at compile time
+// static INTERPRETERS: phf::Map<&'static str, &'static str> = ...;
+// interpreters that map to multiple languages are split with a pipe
+// ex/ "parrot" => "Parrot Assembly|Parrot Internal Representation"
+include!(concat!(env!("OUT_DIR"), "/interpreter-language-map.rs"));
+
+pub fn get_language_by_filename(filename: &str) -> Option<&&'static str> {
+    FILENAMES.get(filename)
+}
+
+pub fn get_language_by_shebang<R: Read>(reader: R) -> Result<Vec<&'static str>, io::Error> {
+    let mut reader = BufReader::new(reader);
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+    if !line.starts_with("#!") {
+        return Ok(vec![]);
+    }
+
+    let languages = line
+        .split("/")
+        .last()
+        .and_then(|interpreter_line| {
+            let mut splits = interpreter_line.split_whitespace();
+            match splits.next() {
+                // #!/usr/bin/env python
+                Some("env") => splits.next(),
+                // #!/usr/bin/python
+                Some(interpreter) => Some(interpreter),
+                // #!
+                None => None,
+            }
+        })
+        .and_then(|interpreter| {
+            //TODO: Lazy Static regex
+            // #!/usr/bin/python2.6.3 -> #!/usr/bin/python2
+            let rg = Regex::new(r#"[0-9]\.[0-9]"#).unwrap();
+            let interpreter = rg.split(interpreter).next().unwrap();
+            INTERPRETERS.get(interpreter)
+        });
+
+    match languages {
+        Some(languages) => Ok(languages.split("|").collect()),
+        None => Ok(Vec::new()),
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Cursor;
 
     #[test]
     fn test_get_language_by_filename() {
-        assert_eq!(get_language_by_filename("APKBUILD"), Some("Alpine Abuild"));
+        assert_eq!(get_language_by_filename("APKBUILD"), Some(&"Alpine Abuild"));
+    }
+
+    #[test]
+    fn test_get_language_by_shebang() {
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("#!/usr/bin/python")).unwrap(),
+            vec!["Python"]
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("#!/usr/bin/env node")).unwrap(),
+            vec!["JavaScript"]
+        );
+
+        let mut parrot_langs = get_language_by_shebang(Cursor::new("#!/usr/bin/parrot")).unwrap();
+        parrot_langs.sort();
+        assert_eq!(
+            parrot_langs,
+            vec!["Parrot Assembly", "Parrot Internal Representation"]
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("#!/usr/bin/python2.6")).unwrap(),
+            vec!["Python"]
+        );
+
+        let empty_vec: Vec<&'static str> = Vec::new();
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("#!/usr/bin/env")).unwrap(),
+            empty_vec
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("#!")).unwrap(),
+            empty_vec
+        );
+
+        assert_eq!(get_language_by_shebang(Cursor::new("")).unwrap(), empty_vec);
+        assert_eq!(
+            get_language_by_shebang(Cursor::new("aslkdfjas;ldk")).unwrap(),
+            empty_vec
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new(" #!/usr/bin/python")).unwrap(),
+            empty_vec
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new(" #!/usr/bin/ ")).unwrap(),
+            empty_vec
+        );
+
+        assert_eq!(
+            get_language_by_shebang(Cursor::new(" #!/usr/bin")).unwrap(),
+            empty_vec
+        );
     }
 }
