25cc4056b2dd3623045da9ef33f8d7c8ba25ebdc
Connor Monks <monkslc@gmail.com>
2020-03-23 20:58:30 -0400

    add disambiguation heuristics function

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use pcre2::bytes::Regex as PCRERegex;
 use phf_codegen::Map as PhfMap;
 use serde::Deserialize;
 use std::{
@@ -15,6 +16,129 @@ struct Language {
     extensions: Option<Vec<String>>,
 }
 
+type NamedPatterns = HashMap<String, MaybeMany<String>>;
+#[derive(Deserialize)]
+struct Heuristics {
+    disambiguations: Vec<Disambiguation>,
+    named_patterns: NamedPatterns,
+}
+
+#[derive(Deserialize)]
+struct Disambiguation {
+    extensions: Vec<String>,
+    rules: Vec<RuleDTO>,
+}
+
+impl Disambiguation {
+    fn to_domain_object_code(&self, named_patterns: &NamedPatterns) -> String {
+        let mut rules = String::new();
+        for rule in self.rules.iter() {
+            rules.push_str(format!("{},", rule.to_domain_object_code(named_patterns)).as_str());
+        }
+        format!("&[{}]", rules)
+    }
+}
+
+#[derive(Deserialize)]
+struct RuleDTO {
+    language: MaybeMany<String>,
+    #[serde(flatten)]
+    pattern: Option<PatternDTO>,
+}
+
+impl RuleDTO {
+    fn to_domain_object_code(&self, named_patterns: &NamedPatterns) -> String {
+        // If we have more than one language, take the first
+        // The only case this happens is the [Linux Kernel Module, AMPL] for .mod extension
+        // And I'm not sure what the right behavior is in that case
+        let language = match &self.language {
+            MaybeMany::Many(values) => &values[0],
+            MaybeMany::One(value) => value,
+        };
+
+        let pattern_code = match &self.pattern {
+            Some(pattern) => format!("Some({})", pattern.to_domain_object_code(named_patterns)),
+            None => String::from("None"),
+        };
+
+        format!(
+            "Rule {{ language: \"{}\", pattern: {}}}",
+            language, pattern_code
+        )
+    }
+}
+
+#[derive(Clone, Deserialize)]
+enum PatternDTO {
+    #[serde(rename = "and")]
+    And(Vec<PatternDTO>),
+    #[serde(rename = "named_pattern")]
+    Named(String),
+    #[serde(rename = "negative_pattern")]
+    Negative(String),
+    #[serde(rename = "pattern")]
+    Positive(MaybeMany<String>),
+}
+
+impl PatternDTO {
+    fn to_domain_object_code(&self, named_patterns: &NamedPatterns) -> String {
+        match self {
+            PatternDTO::Positive(MaybeMany::One(pattern)) => {
+                // Panic on invalid regex now so we can unwrap in lib
+                if let Err(e) = PCRERegex::new(pattern) {
+                    panic!("Invalid regex pattern: {}\n{}", pattern, e);
+                }
+                format!("Pattern::Positive({:?})", pattern)
+            }
+            PatternDTO::Negative(pattern) => {
+                // Panic on invalid regex now so we can unwrap in lib
+                if let Err(e) = PCRERegex::new(pattern) {
+                    panic!("Invalid regex pattern: {}\n{}", pattern, e);
+                }
+                format!("Pattern::Negative({:?})", pattern)
+            }
+            PatternDTO::Positive(MaybeMany::Many(patterns)) => {
+                let mut code = String::from("Pattern::Or(&[");
+                for pattern in patterns.iter() {
+                    let p = PatternDTO::Positive(MaybeMany::One(pattern.clone()));
+                    code.push_str(format!("{},", p.to_domain_object_code(named_patterns)).as_str());
+                }
+                code.push_str("])");
+                code
+            }
+            PatternDTO::And(patterns) => {
+                let mut code = String::from("Pattern::And(&[");
+                for pattern in patterns.iter() {
+                    code.push_str(
+                        format!("{},", pattern.to_domain_object_code(named_patterns)).as_str(),
+                    );
+                }
+                code.push_str("])");
+                code
+            }
+            PatternDTO::Named(pattern_name) => {
+                if let Some(pattern) = named_patterns.get(pattern_name) {
+                    // Assume that all named patterns are positive
+                    let pattern = PatternDTO::Positive(pattern.clone());
+                    return pattern.to_domain_object_code(named_patterns);
+                } else {
+                    panic!(
+                        "Named pattern: {} not found in named pattern map",
+                        pattern_name
+                    );
+                };
+            }
+        }
+    }
+}
+
+#[derive(Clone, Deserialize)]
+#[serde(untagged)]
+enum MaybeMany<T> {
+    Many(Vec<T>),
+    One(T),
+}
+
 fn main() {
     let languages: HashMap<String, Language> =
         serde_yaml::from_str(&fs::read_to_string("languages.yml").unwrap()[..]).unwrap();
@@ -22,6 +146,10 @@ fn main() {
     create_filename_map(&languages);
     create_interpreter_map(&languages);
     create_extension_map(&languages);
+
+    let heuristics: Heuristics =
+        serde_yaml::from_str(&fs::read_to_string("heuristics.yml").unwrap()[..]).unwrap();
+    create_disambiguation_heuristics_map(&heuristics);
 }
 
 fn create_filename_map(languages: &HashMap<String, Language>) {
@@ -111,6 +239,35 @@ fn create_extension_map(languages: &HashMap<String, Language>) {
         &mut file,
         "static EXTENSIONS: phf::Map<&'static str, &[&str]> = \n{};\n",
         extension_to_language_map.build()
+    )
+    .unwrap();
+}
+
+fn create_disambiguation_heuristics_map(heuristics: &Heuristics) {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("disambiguation-heuristics-map.rs");
+    let mut file = BufWriter::new(File::create(&path).unwrap());
+
+    let mut temp_map: HashMap<String, String> = HashMap::new();
+    for dis in heuristics.disambiguations.iter() {
+        for ext in dis.extensions.iter() {
+            let mut extension = ext.clone();
+            // .js => js
+            extension.remove(0);
+            let key = format!("{}", extension);
+            let value = format!("{}", dis.to_domain_object_code(&heuristics.named_patterns));
+            temp_map.insert(key, value);
+        }
+    }
+
+    let mut disambiguation_heuristic_map = PhfMap::new();
+    for (key, value) in temp_map.iter() {
+        disambiguation_heuristic_map.entry(key.as_str(), value.as_str());
+    }
+
+    writeln!(
+        &mut file,
+        "static DISAMBIGUATIONS: phf::Map<&'static str, &'static [Rule]> = \n{};\n",
+        disambiguation_heuristic_map.build()
     )
     .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use lazy_static::lazy_static;
+use pcre2::bytes::Regex as PCRERegex;
 use regex::Regex;
 use std::io::{self, BufRead, BufReader, Read};
 
@@ -13,6 +14,44 @@ include!(concat!(env!("OUT_DIR"), "/interpreter-language-map.rs"));
 // Include the map from interpreters to languages at compile time
 // static EXTENSIONS: phf::Map<&'static str, &[&str]> = ...;
 include!(concat!(env!("OUT_DIR"), "/extension-language-map.rs"));
+
+// Include the map from interpreters to languages at compile time
+// static DISAMBIGUATIONS: phf::Map<&'static str, Rule> = ...;
+include!(concat!(
+    env!("OUT_DIR"),
+    "/disambiguation-heuristics-map.rs"
+));
+
+#[derive(Debug)]
+enum Pattern {
+    And(&'static [Pattern]),
+    Negative(&'static str),
+    Or(&'static [Pattern]),
+    Positive(&'static str),
+}
+
+#[derive(Debug)]
+struct Rule {
+    language: &'static str,
+    pattern: Option<Pattern>,
+}
+
+impl Pattern {
+    fn matches(&self, content: &str) -> bool {
+        match self {
+            Pattern::Positive(pattern) => {
+                let regex = PCRERegex::new(pattern).unwrap();
+                regex.is_match(content.as_bytes()).unwrap_or(false)
+            }
+            Pattern::Negative(pattern) => {
+                let regex = PCRERegex::new(pattern).unwrap();
+                !regex.is_match(content.as_bytes()).unwrap_or(true)
+            }
+            Pattern::Or(patterns) => patterns.iter().any(|pattern| pattern.matches(content)),
+            Pattern::And(patterns) => patterns.iter().all(|pattern| pattern.matches(content)),
+        }
+    }
+}
 
 pub fn get_language_by_filename(filename: &str) -> Option<&&'static str> {
     FILENAMES.get(filename)
@@ -66,6 +105,31 @@ pub fn get_language_by_extension(filename: &str) -> Vec<&'static str> {
     match languages {
         Some(languages) => languages,
         None => vec![],
+    }
+}
+
+pub fn disambiguate_extension_overlap(
+    extension: &str,
+    candidates: &Vec<&'static str>,
+    content: &str,
+) -> Option<&'static str> {
+    match DISAMBIGUATIONS.get(extension) {
+        Some(rules) => {
+            for rule in rules.iter() {
+                if candidates.contains(&rule.language) {
+                    if let Some(pattern) = &rule.pattern {
+                        if pattern.matches(content) {
+                            return Some(rule.language);
+                        };
+                    } else {
+                        // if there is not a pattern its a match by default
+                        return Some(rule.language);
+                    };
+                };
+            }
+            None
+        }
+        None => None,
     }
 }
 
@@ -166,5 +230,72 @@ mod tests {
         assert_eq!(get_language_by_extension(".c"), empty_vec);
         assert_eq!(get_language_by_extension(""), empty_vec);
         assert_eq!(get_language_by_extension("noextension"), empty_vec);
+    }
+
+    #[test]
+    fn test_disambiguate_extension_overlap() {
+        // Matches Positive Pattern
+        assert_eq!(
+            disambiguate_extension_overlap("es", &vec!["Erlang", "JavaScript"], "'use strict';"),
+            Some("JavaScript")
+        );
+
+        // Matches Negative Pattern
+        assert_eq!(
+            disambiguate_extension_overlap(
+                "sql",
+                &vec!["PLSQL", "PLpgSQL", "SQL", "SQLPL", "TSQL"],
+                "LALA THIS IS SQL"
+            ),
+            Some("SQL")
+        );
+
+        // Matches And with all positives
+        assert_eq!(
+            disambiguate_extension_overlap(
+                "pro",
+                &vec!["Proguard", "Prolog", "INI", "QMake", "IDL"],
+                "HEADERS SOURCES"
+            ),
+            Some("QMake")
+        );
+
+        // Doesn't match And if less than all match
+        assert_eq!(
+            disambiguate_extension_overlap(
+                "pro",
+                &vec!["Proguard", "Prolog", "INI", "QMake", "IDL"],
+                "HEADERS"
+            ),
+            None
+        );
+
+        // Matches And with negative pattern
+        assert_eq!(
+            disambiguate_extension_overlap(
+                "ms",
+                &vec!["Roff", "Unix Assembly", "MAXScript"],
+                ".include:"
+            ),
+            Some("Unix Assembly")
+        );
+
+        // Matches Or if one is true
+        assert_eq!(
+            disambiguate_extension_overlap("p", &vec!["Gnuplot", "OpenEdge ABL"], "plot"),
+            Some("Gnuplot")
+        );
+
+        // Matches named pattern
+        assert_eq!(
+            disambiguate_extension_overlap("h", &vec!["Objective-C", "C++"], "std::out"),
+            Some("C++")
+        );
+
+        // Matches default language pattern (no pattern specified)
+        assert_eq!(
+            disambiguate_extension_overlap("man", &vec!["Roff Manpage", "Roff"], "alskdjfahij"),
+            Some("Roff")
+        );
     }
 }
